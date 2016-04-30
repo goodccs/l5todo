ea9c28cacb0cc43702c618558e2d49baf46f919b
Jean Parra <jcprock@hotmail.com>
2016-04-30 14:01:53 -0430

    Cambios Index Redirect

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,8 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    #return view('welcome');
+    return redirect('/projects');
 });
 
 // Provide controller methods with object instead of ID
